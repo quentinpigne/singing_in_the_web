0933eb96b344e2438d09945a8db271b745857f88
Emma Spalart <emma.spalart@ensimag.grenoble-inp.fr>
2016-02-02 11:16:35 +0100

    Ergonomie de l'interface admin

--- songs/admin.py
+++ songs/admin.py
@@ -1,12 +1,58 @@
+#-*- coding: utf-8 -*-
 from django.contrib import admin
-from songs.models import Album, Artist, Artistterm, Listener, Musicbrainztag, Song, Songstatistic, Tag
+from songs.models import Album, Artist, Similarity, Song, Songstatistic
+
+class AlbumAdmin(admin.ModelAdmin):
+    exclude = ('album_id', )
+    fieldsets = (
+                (None, {
+                        'fields': ('album_name', 'songs')
+                        }
+                 ),
+                ('Autres propriétés', {
+                                       'classes': ('collapse', ), 'fields': ('album_year', 'album_7digitalid') 
+                                       }
+                 
+                 )
+                )
+
+class ArtistAdmin(admin.ModelAdmin):
+    fieldsets = (
+                 (None, {
+                         'fields': ('artist_id', 'artist_name', 'songs')
+                         }
+                  ),
+                 ('Autres informations', {
+                                       'classes': ('collapse', ), 'fields': ('artist_familiarity', 'artist_hotness', 'artist_latitude', 'artist_longitude', 'artist_location', 'similar_artists') 
+                                       }
+                 
+                 ),
+                 ('Identifiants', {
+                                   'classes': ('collapse', ), 'fields': ('artist_mbid', 'artist_playmeid', 'artist_7digitalid')
+                                   }
+                  
+                  )
+                 )
+    
+class SimilarityInline(admin.TabularInline):
+    model = Similarity
+    fk_name = 'song1'
+    extra = 1
+        
+class SongAdmin(admin.ModelAdmin):
+    fieldsets = (
+                 (
+                  None, {
+                         'fields': ('song_id', 'track_id', 'title')
+                         }
+                  ),
+                 ('Autres informations', {
+                                          'classes': ('collapse', ), 'fields': ('num_songs', 'track_7digitalid', 'song_hotness')
+                                          })
+                 )
+    inlines = (SimilarityInline, )
 
 # Register your models here.
-admin.site.register(Album)
-admin.site.register(Artist)
-admin.site.register(Artistterm)
-admin.site.register(Listener)
-admin.site.register(Musicbrainztag)
-admin.site.register(Song)
-admin.site.register(Songstatistic)
-admin.site.register(Tag)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Song, SongAdmin)  
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
@@ -6,37 +7,39 @@ from django.db.models.fields.related import ForeignKey
 
 class Album(models.Model):
     album_id = models.AutoField(primary_key=True)
-    album_name = models.CharField(max_length=1024)
-    album_year = models.IntegerField(blank=True, null=True)
-    album_7digitalid = models.IntegerField(blank=True, null=True)
-    songs = models.ManyToManyField('Song')
+    album_name = models.CharField(max_length=1024, verbose_name='Titre')
+    album_year = models.IntegerField(blank=True, null=True, verbose_name='Année de sortie')
+    album_7digitalid = models.IntegerField(blank=True, null=True, verbose_name='Identifiant 7digital')
+    songs = models.ManyToManyField('Song', verbose_name='Chansons')
     
     def __unicode__ (self):
         return self.album_name
 
     class Meta:
         db_table = 'Album'
+        verbose_name='Album'
 
 
 class Artist(models.Model):
-    artist_id = models.CharField(max_length=32, primary_key=True)
-    artist_mbid = models.CharField(max_length=40)
-    artist_playmeid = models.IntegerField()
-    artist_name = models.CharField(max_length=1024)
-    artist_familiarity = models.FloatField(blank=True, null=True)
-    artist_hotness = models.FloatField(blank=True, null=True)
-    artist_latitude = models.FloatField(blank=True, null=True)
-    artist_longitude = models.FloatField(blank=True, null=True)
-    artist_location = models.CharField(max_length=1024, blank=True, null=True)
-    artist_7digitalid = models.IntegerField()
-    similar_artists = models.ManyToManyField('Artist')
-    songs = models.ManyToManyField('Song')
+    artist_id = models.CharField(max_length=32, primary_key=True, verbose_name='Identifiant')
+    artist_mbid = models.CharField(max_length=40, blank=True, null=True, verbose_name='Identifiant Musicbrainz')
+    artist_playmeid = models.IntegerField(blank=True, null=True, verbose_name='Identifiant Playme')
+    artist_name = models.CharField(max_length=1024, verbose_name='Nom')
+    artist_familiarity = models.FloatField(blank=True, null=True, verbose_name='Popularité')
+    artist_hotness = models.FloatField(blank=True, null=True, verbose_name='Hotness')
+    artist_latitude = models.FloatField(blank=True, null=True, verbose_name='Latitude')
+    artist_longitude = models.FloatField(blank=True, null=True, verbose_name='Longitude')
+    artist_location = models.CharField(max_length=1024, blank=True, null=True, verbose_name='Localisation')
+    artist_7digitalid = models.IntegerField(blank=True, null=True, verbose_name='Identifiant 7digital')
+    similar_artists = models.ManyToManyField('Artist', blank=True, verbose_name='Artistes similaires')
+    songs = models.ManyToManyField('Song', blank=True, verbose_name='Chansons')
     
     def __unicode__ (self):
         return self.artist_name
     
     class Meta:
         db_table = "Artist"
+        verbose_name='Artiste'
 
 
 class Artistterm(models.Model):
@@ -56,7 +59,7 @@ class Artistterm(models.Model):
 class Musicbrainztag(models.Model):
     tag_id = models.AutoField(primary_key=True)
     artist = models.ForeignKey('Artist')
-    tag = models.CharField(max_length=40)
+    tag = models.CharField(max_length=255)
     tag_count = models.IntegerField()
     
     def __unicode__ (self):
@@ -67,19 +70,20 @@ class Musicbrainztag(models.Model):
 
 
 class Song(models.Model):
-    song_id = models.CharField(primary_key=True, max_length=32)
-    title = models.CharField(max_length=1024)
-    num_songs = models.IntegerField(blank=True, null=True)
-    track_id = models.CharField(max_length=32, blank=True, null=True)
-    track_7digitalid = models.IntegerField(blank=True, null=True)
-    song_hotness = models.FloatField(blank=True, null=True)
-    similar_songs = models.ManyToManyField('Song', through='Similarity')
+    song_id = models.CharField(primary_key=True, max_length=32, verbose_name='Identifiant')
+    track_id = models.CharField(max_length=32, verbose_name='Autre identifiant')
+    title = models.CharField(max_length=1024, verbose_name='Titre')
+    num_songs = models.IntegerField(blank=True, null=True, verbose_name='Nombre de chansons')
+    track_7digitalid = models.IntegerField(blank=True, null=True, verbose_name='Indentifaint 7digital')
+    song_hotness = models.FloatField(blank=True, null=True, verbose_name='Hotness')
+    similar_songs = models.ManyToManyField('Song', through='Similarity', blank=True, verbose_name='Chansons similaires')
     
     def __unicode__ (self):
         return self.title
 
     class Meta:
         db_table = 'Song'
+        verbose_name='Chanson'
 
 
 class Songstatistic(models.Model):
@@ -120,7 +124,7 @@ class Listening(models.Model):
 
 class Listener(models.Model):
     listener_id = models.CharField(primary_key=True, max_length=40)
-    songs = models.ManyToManyField('Song', through='Listening')
+    songs = models.ManyToManyField('Song', through='Listening', blank=True)
     
     def __unicode__ (self):
         return self.listener_id
@@ -130,7 +134,7 @@ class Listener(models.Model):
         
 class Tag(models.Model):
     tag_id = models.AutoField(primary_key=True)
-    tag = models.CharField(max_length=40)
+    tag = models.CharField(max_length=255)
     tag_count = models.IntegerField()
     songs = models.ManyToManyField('Song')
     
@@ -142,9 +146,10 @@ class Tag(models.Model):
         db_table = 'Tag'    
         
 class Similarity(models.Model):
-    song1 = ForeignKey('Song', related_name='%(class)s_similarity_song1')
-    song2 = ForeignKey('Song', related_name='%(class)s_similarity_song2')
+    song1 = ForeignKey('Song', related_name='%(class)s_similarity_song1', verbose_name='Chanson')
+    song2 = ForeignKey('Song', related_name='%(class)s_similarity_song2', verbose_name='Chanson')
     similarity = models.FloatField()
     
     class Meta:
         db_table = 'Similarity'
+        verbose_name = 'Chansons Similaire'
