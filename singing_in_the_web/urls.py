e49bd8326756cba6ac99d1d64dcd853a995423b4
danversx <steven.durrenmath@gmail.com>
2016-02-02 01:20:10 +0100

    Just one page

--- singing_in_the_web/urls.py
+++ singing_in_the_web/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('songs.urls')),
+    url(r'^songs/', include('songs.urls')),
     url(r'^admin/', admin.site.urls),
 ]
