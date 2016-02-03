5b55edfed1d2eb9d0d4c33effd2a0a9b92ce8285
Emma Spalart <emma.spalart@ensimag.grenoble-inp.fr>
2016-02-03 14:56:44 +0100

    Petite modif sur les urls

--- songs/urls.py
+++ songs/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^artist_details/$', views.artist_details, name='artist_details'),
     url(r'^song_details/$', views.song_details, name='song_details'),
     url(r'^album_details/$', views.album_details, name='album_details'),
-    url(r'^artist_relatives', views.artist_relatives, name='arist_relatives'),
-    url(r'^song_relatives', views.song_relatives, name='song_relatives'),
-    url(r'^album_relatives', views.album_relatives, name='album_relatives')
+    url(r'^artist_relatives/$', views.artist_relatives, name='arist_relatives'),
+    url(r'^song_relatives/$', views.song_relatives, name='song_relatives'),
+    url(r'^album_relatives/$', views.album_relatives, name='album_relatives')
 ]
